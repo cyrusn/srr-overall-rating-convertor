4b2e0d638249b7d816ecc2d922e24baee2affc4a
Cyrus Ngan <cyrusncy@gmail.com>
2026-03-08 19:42:37 +0800

    reorder config.py

--- v2/config.py
+++ v2/config.py
@@ -11,9 +11,13 @@ GOOGLE_KEY_FILE = ".env.key.json"
 # Sheet Names (Input Data)
 SHEET_STUDENTS = "Students"
 SHEET_SUBJECT_INFO = "SubjectInfo"
-SHEET_F6_REPORT = "F6Report"
+
+# Report Card
 SHEET_F5_T1_REPORT = "F5T1Report"
 SHEET_F5_T2_REPORT = "F5T2Report"
+SHEET_F6_REPORT = "F6Report"
+
+# Predicted Grade
 SHEET_F5_T1_GRADE = "F5T1Grade"
 SHEET_F5_T2_GRADE = "F5T2Grade"
 SHEET_F6_GRADE = "F6Grade"
